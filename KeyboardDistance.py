ea6ef656dbdd17fd42ac3465f9d460685456dfd1
RishabhPatil <rishabhpatil@gmail.com>
2016-11-07 16:48:04 +0530

    Added Comments

--- KeyboardDistance.py
+++ KeyboardDistance.py
@@ -1,8 +1,8 @@
-kb=["1234567890-=",
+kb=["1234567890-=",  #Array Of Keyboard Keys
     "qwertyuiop[]\\",
     "asdfghjkl;'",
     "zxcvbnm,./"]
-skb=["!@#$%^&*()_+",
+skb=["!@#$%^&*()_+", #Same keys +Shift
      "QWERTYUIOP{}|",
      "ASDFGHJKL:\"",
      "ZXCVBNM<>?"]
@@ -15,5 +15,5 @@ for i in range(len(kb)):
 def dist(a,b):
     y1,x1=keys[a]
     y2,x2=keys[b]
-    d=float(float((((x1-x2)**2)+((y1-y2)**2)))**(0.5))
+    d=float(float((((x1-x2)**2)+((y1-y2)**2)))**(0.5))  #Euclidean Distance
     print d
