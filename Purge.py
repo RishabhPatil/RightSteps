6b42bb3e1b50fcbe20965e2fc47f91836052eafa
RishabhPatil <rishabhpatil@gmail.com>
2016-11-13 16:07:15 +0530

    Data Cleaned and Formatted

--- Purge.py
+++ Purge.py
@@ -1,33 +1,56 @@
-import csv
+import re
 
 def compare(t,n):
-	c=0
-	k='y'
-	for i in data[n]:
-		for j in range(len(i)):
-			if i[j]==t[j]:
-				c=c+1
-		if c>2:
-			print t,i
-			#k=raw_input("Add new?")
-			break # RETURN SOMETHING MAYBE???
-	if k=='y':
-		data[n].add(tuple(t))
+        c=0
+        k='y'
+        if data[n]:
+                for i in data[n]:
+                        for j in range(len(i)):
+                                if i[j]==t[j]:
+                                        c=c+1
+                        if c==4:
+                                k='n'
+        if k=='y':
+                data[n].append(tuple(t))
 
-data=[set() for i in range(27)]
+data=[list() for i in range(27)]
+print data
+#Read from CSV
 with open('data.csv', 'rb') as f:
-	df=f.read()
-	df=df.split("\n")
-	for i in df:
-		t=i.split(',')
-		if len(t[0])!=0:
-			t[0]=t[0].lstrip()
-			j=ord(t[0][0].lower())-97
-			compare(t,j)
-		else:
-			compare(t,26)
-		#x=raw_input()
+        df=f.read()
+        df=df.split("\n")
+        for i in df:
+#Cleanse Data
+                i=re.sub('\\r','',i)
+                t=i.split(',')
+                j=27
+                t[0]=(t[0].lstrip()).rstrip()
+                if len(t[0])!=0:
+                        j=ord(t[0][0].lower())-97
+                else:
+                        j=26
+                t[0]=re.sub('[^a-z ]+','',t[0].lower())             #[1][2]
+                t[0]=t[0].split(' ')
+                t[1]=t[1].lower()
+                t[1]=t[1].split('@')
+                t[2]=re.sub('[^0-9]+','',t[2].lower())             #[3][4]
+                t[3]=t[3].lower()
+                t[3]=t[3].split(' ')
+#Compare with group based on 1st alphabet
+                compare(t,j)
 
-# (Name:{Yash Kale, YK},PH:{23443},EMAIL:{23vcc0},ADD:{asdafaf, asd})
+"""
+FINAL FORMAT:
+(['tejas', 'dhanepkar'], ['tejasdhanepkar25', 'gmail.com'], '7038810983', ['428', 'kearsley', 'st'])
+"""
+
 for i in range(27):
-	print data[i]
+        print data[i]
+
+"""
+ISSUES:
+[1] RE support for all languages (Unicode) : http://stackoverflow.com/questions/7206499/
+[2] Current RE replaces '-' with '' so hyphenated strings are appended. Other similar issues?
+[3] Phone numbers with calling codes? Split on 1st ' ' after '+'?
+[4] Calling codes are always prefixes, so we can compare in reverse order?
+"""
